eadca2fb1962aa028cfbd657b92e35f48c34c3f7
unknown <you@example.com>
2021-01-17 23:15:43 +0300

    3000 port

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management.commands.runserver import Command
 
 def main():
     """Run administrative tasks."""
@@ -20,3 +20,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
