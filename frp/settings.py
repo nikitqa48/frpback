c8029aa390096b9a599e5546375c11e7e0ecf26f
unknown <you@example.com>
2021-02-17 18:39:22 +0300

    media

--- frp/settings.py
+++ frp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&-k7e!c9=d@6@@oq8cczum4l+o3gz(i3*^bd@z7$0&e)v(joj9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -134,7 +134,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = '/home/invest/marketplace/frpback/media/'
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
