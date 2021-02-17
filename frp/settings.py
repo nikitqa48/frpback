279686273925e542fbfeb05ce6bd38bd00f5e4f2
unknown <you@example.com>
2021-02-17 13:25:43 +0300

    static

--- frp/settings.py
+++ frp/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATIC_DIR = [
+    os.path.join(BASE_DIR, "static"),
+    "/home/invest/marketplace/frpback/static/"
+]
 STATICFILES_DIRS = [STATIC_DIR]   
 # STATIC_ROOT = "/home/invest/marketplace/frpback/static/"
