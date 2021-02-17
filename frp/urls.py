36e5d1fbda5fa08c3a7df7b52cb80b008a5353e6
unknown <you@example.com>
2021-02-17 19:59:15 +0300

    media

--- frp/urls.py
+++ frp/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIAROOT) + [
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('admin/', admin.site.urls),
     path('api/', include('branch.urls'))
 ]
